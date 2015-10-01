f492cadaf84f8f7f9874573f589cd4492af83e90
Martin Evans <martindevans@gmail.com>
2015-10-01 02:28:52 +0100

    Added a test project with a very basic unit test just to confirm loading DLLs works in a test project
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ SupersonicSound.Test/Playground.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupersonicSound.LowLevel;
+using SupersonicSound.Wrapper;
+
+namespace SupersonicSound.Test
+{
+    [TestClass]
+    public class Playground
+    {
+        private void TestInitSystem(IPreInitilizeLowLevelSystem sys)
+        {
+            sys.Output = OutputMode.NoSoundNotRealtime;
+        }
+
+        [TestMethod]
+        public void CreateSystem()
+        {
+            using (var sys = new LowLevelSystem(preInit: TestInitSystem))
+            {
+            }
+        }
+    }
+}
