8d370041ecb392a869bd7ebdc2f1fbc25707099a
Linus Lindgren <linlind@student.chalmers.se>
2017-10-10 20:43:44 +0200

    Raised FMOD version to 1.10

--- SupersonicSound/Wrapper/Fmod/fmod.cs
+++ SupersonicSound/Wrapper/Fmod/fmod.cs
@@ -16,7 +16,7 @@ namespace FMOD
     */
     public class VERSION
     {
-        public const int    number = 0x00010700;
+        public const int number = 0x00011000;
 #if X64
         public const string dll    = "fmod64";
 #else
